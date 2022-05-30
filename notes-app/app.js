dab919d4382ed492d560d18b32b48b952938cb09
Code4Blessings <robincw35@gmail.com>
2022-05-30 11:38:12 -0400

    Deleted utils file and updated app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,10 +7,4 @@ const notes = getNotes()
 console.log(sum)
 console.log(notes)
 
-//Challenge: Define and use a function in a new file
-
-//1. Create a new file called notes.js
-//2. Create a getNotes function that returns "Your notes.."
-//3. Export getNotes function
-//4. From app/js load in and call the function printing message to console
 
--- notes-app/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('utils.js')
-
-const firstName = 'Robin';
-
-const add = function(a,b) {
-    return a + b
-}
-
-module.exports = add
