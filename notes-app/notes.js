11872fd04d0f489698bca2579795c679f1d738f5
Code4Blessings <robincw35@gmail.com>
2022-06-04 06:31:59 -0400

    Updated notes.js file

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,7 +1,34 @@
-console.log('notes.js')
+const fs = require('fs')
+
 
 const getNotes = function() {
     return 'Your notes...';
 }
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    notes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(notes)
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = dataBuffer
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toSting()
+        return JSON.parse(dataJSON)
+    }catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
